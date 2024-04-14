53d836b7fd06874d6d47c02f360389a1453e0b09
Rip derek <castillosteven09@outlook.es>
2024-04-13 20:44:14 -0500

    Envio de correos adm users

--- SRC/controllers/Secciones/secciones-controller.js
+++ SRC/controllers/Secciones/secciones-controller.js
@@ -109,7 +109,65 @@ const agg_invitado_seccion = async (req, res, next) => {
     return res.status(404).json({ error: error.message });
   }
 };
+//funcion que retorne los participantes de un test
+const participantes_test_id_test = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      "select * from participantes_test_id_test($1)",
+      [id]
+    );
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
+const informacion_participante_test = async (req, res, next) => {
+  try {
+    const { id_test, tokenUsuar } = req.params;
+    const result = await pool.query(
+      "select * from informacion_seccion_usuario($1,$2)",
+      [id_test, tokenUsuar]
+    );
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: error.message });
+  }
+};
+//funcion para eliminar a un participante de una seccion skere modo diablo
+const eliminar_usuario_seccion = async (req, res, next) => {
+  try {
+    //const { id } = req.params;
+    const { p_id_seccion, p_id_usuario_invi } = req.body;
+    const result = await pool.query("call eliminar_usuario_seccion($1,$2)", [
+      p_id_usuario_invi,
+      p_id_seccion,
+    ]);
+    return res
+      .status(200)
+      .json({ message: "Se elimino al usuario de la seccion" });
+    //return res.status(200).json(result.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ error: error.message });
+  }
+};
+//buscar usuarios
+const search_usuarios = async (req, res, next) => {
+  try {
+    const { palabra_clave } = req.params;
+    const result = await pool.query("select * from search_users($1)", [
+      palabra_clave,
+    ]);
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: error.message });
+  }
+};
 
+//funcion para obtener informacion del usuario y de la seccion para saber si es admin o no skere modo diablo
 module.exports = {
   secciones_usuario,
   crear_seccion_usuario,
@@ -118,4 +176,8 @@ module.exports = {
   data_editable,
   editar_seccion_op,
   agg_invitado_seccion,
+  participantes_test_id_test,
+  informacion_participante_test,
+  eliminar_usuario_seccion,
+  search_usuarios,
 };
--- SRC/controllers/Usuarios/usuario-controller.js
+++ SRC/controllers/Usuarios/usuario-controller.js
@@ -1,48 +1,61 @@
-const pool = require('../../db');
-
+const pool = require("../../db");
 
 const datos_Usuarios = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const result = await pool.query('select * from FU_usuario_data($1)', [id]);
-        console.log(id);
-        console.log(result.rows[0]);
+    const result = await pool.query("select * from FU_usuario_data($1)", [id]);
+    console.log(id);
+    console.log(result.rows[0]);
 
-        return res.status(200).json(result.rows[0]);
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
-    }
-}
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
 const crear_usuario = async (req, res, next) => {
-    try {
-        //const { id } = req.params;
-        const { p_contraseña, p_correo_institucional, p_identificacion, p_nombres_apellidos, p_celular, p_tipo_identificacion } = req.body;
-        const result = await pool.query('call crear_usuario($1,$2,$3,$4,$5,$6)', [p_contraseña, p_correo_institucional, p_identificacion, p_nombres_apellidos, p_celular, p_tipo_identificacion]);
-        return res.status(200).json({ message: "Se creó un Usuario" });
-        //return res.status(200).json(result.rows);
-    } catch (error) {
-        console.log(error);
-        return res.status(404).json({ error: error.message });
-    }
-}
-//funcion que devuelve las caracteristicas de la interfaz de un usuario 
+  try {
+    //const { id } = req.params;
+    const {
+      p_contrasena,
+      p_correo_institucional,
+      p_identificacion,
+      p_nombres_apellidos,
+      p_celular,
+    } = req.body;
+    const result = await pool.query("call crear_usuario($1,$2,$3,$4,$5)", [
+      p_contrasena,
+      p_correo_institucional,
+      p_identificacion,
+      p_nombres_apellidos,
+      p_celular,
+    ]);
+    return res.status(200).json({ message: "Se creó un Usuario" });
+    //return res.status(200).json(result.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ error: error.message });
+  }
+};
+//funcion que devuelve las caracteristicas de la interfaz de un usuario
 //FU_interfaz_usuario
 const interfaz_usuario = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const result = await pool.query('select * from FU_interfaz_usuario($1)', [id]);
-        console.log(id);
-        console.log(result.rows[0]);
+    const result = await pool.query("select * from FU_interfaz_usuario($1)", [
+      id,
+    ]);
+    console.log(id);
+    console.log(result.rows[0]);
 
-        return res.status(200).json(result.rows[0]);
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
-    }
-}
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
 module.exports = {
-    datos_Usuarios,
-    crear_usuario,
-    interfaz_usuario
+  datos_Usuarios,
+  crear_usuario,
+  interfaz_usuario,
 };
